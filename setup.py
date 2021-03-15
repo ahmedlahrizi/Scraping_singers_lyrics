c785b26a97f9c470a18d82a64eb57290472b9b3f
Ahmed Lahrizi <ahmed.lahrizi05@gmail.com>
2021-03-15 12:23:00 +0100

    deleted somme git bugs

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic script to get most used words by a singer using spacy and requests and Counter'
+
+# Setting up
+setup(
+    name="scrap_singers",
+    version=VERSION,
+    author="Lahrizi Ahmed",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['spacy', 'Counter', 'bs4', 'requests'],
+    keywords=['python', 'singers', 'scrapping', 'web', 'lyrics', 'spacy', 'elon musk'],
+    project_urls={
+        "Bug Tracker": "https://github.com/ahmedlahrizi/Scraping_singers_lyrics",
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.9",
+    package_dir={"": "scrap_singers"}
+)
